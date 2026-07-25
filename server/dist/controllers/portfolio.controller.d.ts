4fd233fed9eb64981bb630c8b399489a7aa559c3
rubel9226 <mdrubelhossen9226@gmail.com>
2026-07-25 21:53:48 +0600

    Downgrade TypeScript

--- server/dist/controllers/portfolio.controller.d.ts
+++ server/dist/controllers/portfolio.controller.d.ts
@@ -1,9 +1,9 @@
 import type { NextFunction, Request, Response } from "express";
 import createError from 'http-errors';
-export declare const handleCreatePortfolio: (req: Request, res: Response, next: NextFunction) => Promise<createError.HttpError<401> | Response<any, Record<string, any>> | undefined>;
-export declare const handleGetPortfolio: (req: Request, res: Response, next: NextFunction) => Promise<createError.HttpError<401> | Response<any, Record<string, any>> | undefined>;
+export declare const handleCreatePortfolio: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | createError.HttpError<401> | undefined>;
+export declare const handleGetPortfolio: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | createError.HttpError<401> | undefined>;
 export declare const handleGetPortfolioPublic: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const handleUpdateAbout: (req: Request, res: Response, next: NextFunction) => Promise<createError.HttpError<401> | Response<any, Record<string, any>> | undefined>;
+export declare const handleUpdateAbout: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | createError.HttpError<401> | undefined>;
 export declare const handleGetPortfolioProjects: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
 export declare const handleGetPortfolioProjectsPublic: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
 //# sourceMappingURL=portfolio.controller.d.ts.map
--- server/dist/controllers/project.controller.d.ts
+++ server/dist/controllers/project.controller.d.ts
@@ -1,6 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import createError from 'http-errors';
-export declare const handleAddProject: (req: Request, res: Response, next: NextFunction) => Promise<createError.HttpError<401> | Response<any, Record<string, any>> | undefined>;
+export declare const handleAddProject: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | createError.HttpError<401> | undefined>;
 export declare const handleGetMyProject: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
 export declare const handleGetAllProject: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
 export declare const handleGetSingleProject: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
